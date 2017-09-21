45c616008af615c3aa8f767d146ed92b6510521d
Mark Berry <mark@EasyNerdy.tx.rr.com>
2017-09-20 22:59:42 -0500

    Pull data for wishlist

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,32 +1,45 @@
 import React from "react";
-
-const wishlist = [
-  {
-    id: 7,
-    title: "Elefante",
-    image: ""
-  },
-  {
-    id: 8,
-    title: "Fishies",
-    image: ""
-  },
-  {
-    id: 9,
-    title: "Giraffe",
-    image: ""
-  }
-]
+import axios from "axios";
 
 class Wishlist extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      wishlistIds: [
+        10,20,30,40
+      ],
+      wishlistData: []
+    }
+  }  
+  
+  componentDidMount() {
+    let newWishlistData =[];
+    this.state.wishlistIds.forEach(id => {
+      axios.get('https://bookshout.com/api/books/' + id + '.json')
+      .then(response => {
+        newWishlistData = this.state.wishlistData.concat({
+          id: response.data.id,
+          title: response.data.title,
+          isbn: response.data.isbn,
+          cover_url: response.data.cover_url
+        });
+        this.setState({ wishlistData: newWishlistData })
+      })
+      .catch(error => {
+        console.error(error);
+      })
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Wishlist</h2>
-        {wishlist.map(book=>{
+        {console.log("the state is: ",this.state.wishlistData)}
+        {this.state.wishlistData.map(book=>{
           return <li key={book.id}>
-            <span>{book.title}</span>
-            <div>{book.image}</div>
+            <div>{book.title}</div>
+            <img src={book.cover_url} width="100" alt="Book Image"/>
           </li>
         })}
       </div>
