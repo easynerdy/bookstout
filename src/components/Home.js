5539fb98905b275eeed416bbc682df497f490d5d
Mark Berry <mark@EasyNerdy.tx.rr.com>
2017-09-20 21:30:53 -0500

    Add search bar and init submitSearch

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,18 +23,41 @@ const searchResults = [
 ]
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  submitSearch(e) {
+    e.preventDefault();
+    console.log("We made it in submitSearch!");
+  }
+
   render() {
+    const component = this;
     return (
     <div>
-      <h2>Search Results</h2>
-      <ul>
-        {searchResults.map(book=>{
-          return <li key={book.id}>
-            <div><Link to={"/details/" + book.id}>{book.title}</Link> {book.price}</div>
-            <div>{book.synopsis}</div>
-          </li>
-        })}
-      </ul>
+      <input type="text"/>
+      <button onClick={component.submitSearch}>Search</button>
+      {this.state.books.length >= 0
+      ? <div>Search for a book!</div>
+      : <div> 
+          <h2>Search Results</h2>
+          <ul>
+            {this.state.books.map(book=>{
+              return <li key={book.id}>
+                <div><Link to={"/details/" + book.id}>{book.title}</Link> {book.price}</div>
+                <div>{book.synopsis}</div>
+              </li>
+            })}
+          </ul>
+        </div>}
     </div>
     )}
 }
